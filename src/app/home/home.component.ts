e189ceadabe920b17d7f07e0206a5e0f2639d885
Abhishek N N <7abhisheknn@gmail.com>
2023-08-29 10:58:40 +0530

    finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,12 +11,12 @@ import { HousingService } from '../housing.service';
   template: `
     <section >
       <form>
-        <input type="text" placeholder="Filter by City" >
-        <button class="primary" type="button" >Search</button>
+        <input type="text" placeholder="Filter by City" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
@@ -24,8 +24,19 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-
+  filteredLocationList: HousingLocation[] = []; 
   constructor() {
     this.housingLocationList = this.housingService.getHousingLocationList();
+    this.filteredLocationList = this.housingLocationList;
   }
+  filterResults(text: string){
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => {
+      return housingLocation.city.toLowerCase().includes(text.toLowerCase());
+    });
+  }
+
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HousingLocation } from './housing-location';
+import { last } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -115,5 +116,9 @@ export class HousingService {
 
   getHousingLocation(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+  
+  submitApplication(firstName: string, lastName: string, email:string) {
+    console.log(firstName, lastName, email);
   }
 }
